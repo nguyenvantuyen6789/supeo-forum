992d1c5389737556be1cb40530aaaa89659daef7
Admin <nguyenvantuyen6789@gmail.com>
2022-02-13 10:07:04 +0700

    Xong! Tạo Dự Án Ban Đầu - Dev & Prod

---
+++ src/main/java/com/tuyennguyen/supeo/forum/SupeoForumApplication.java
@@ -0,0 +1,21 @@
+package com.tuyennguyen.supeo.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SupeoForumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupeoForumApplication.class, args);
+    }
+
+    @GetMapping("show")
+    public String showData() {
+        return "data 2";
+    }
+
+}
---
+++ src/test/java/com/tuyennguyen/supeo/forum/SupeoForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyennguyen.supeo.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupeoForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
