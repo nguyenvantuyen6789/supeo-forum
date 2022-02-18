cbd78e48a7707622a89036bbabce96bac10ed83d
Nguyễn Văn Tuyên <v.tuyennv18@vinhomes.vn>
2022-02-18 17:13:59 +0700

    Xong! Sửa Đúng 1 File Dùng Password Encode Trong Db. Password Trong Db Từ String Generate Sang Chuỗi Loằng Ngoằng

--- src/main/java/com/tuyennguyen/supeo/forum/websecurity/WebSecurityConfig.java
+++ src/main/java/com/tuyennguyen/supeo/forum/websecurity/WebSecurityConfig.java
@@ -1,26 +1,39 @@
 package com.tuyennguyen.supeo.forum.websecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Qualifier("userDetailsSpringFWService")
-    @Autowired
-    UserDetailsService userDetailsService;
+//    @Qualifier("userDetailsSpringFWService")
+//    @Autowired
+//    UserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService);
+//    }
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider
+                = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return  provider;
     }
 
     @Override
